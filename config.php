1e240d4d4ca75951fa4e4fec38652a43dee76362
Yuri Neves <yurineves92@gmail.com>
2019-10-21 01:22:21 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+require "environment.php";
+
+$config = array();
+
+if(ENVIRONMENT == 'development'){
+    define("BASE_URL","http://classificados.localhost.com/");
+    $config['dbname'] = 'classificados';
+    $config['host'] = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = 'password';
+} else {
+    define("BASE_URL","http://meusite.com.br");
+    $config['dbname'] = 'classificados';
+    $config['host'] = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = 'password';
+}
+
+global $db;
+
+try {
+    $db = new PDO("mysql:dbname=".$config['dbname'].";host".$config['host'],$config['dbuser'], $config['dbpass']);
+} catch(PDOException $e) {
+    echo "ERRO: ".$e->getMessage();
+    exit;
+}
